b3263694593c82dbedb4a2037c3aa40eb7a6cf9d
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-02-16 18:16:55 +0100

    fix: foo changed for working with same values in pairs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   const openBrackets = bracketsConfig.map((pair) => pair[0]);
   const closeBrackets = bracketsConfig.map((pair) => pair[1]);
-
   const stack = [];
+
   for (let i = 0; i < str.length; i += 1) {
     let curSymbol = str[i];
+    let topElement = stack[stack.length - 1];
 
     if (openBrackets.includes(curSymbol)) {
-      stack.push(curSymbol);
+      if (
+        stack.length !== 0 &&
+        closeBrackets.indexOf(curSymbol) === openBrackets.indexOf(topElement)
+      ) {
+        stack.pop();
+      } else {
+        stack.push(curSymbol);
+      }
     } else {
       if (stack.length === 0) {
         return false;
       }
-
-      let topElement = stack[stack.length - 1];
-
       if (
         closeBrackets.indexOf(curSymbol) === openBrackets.indexOf(topElement)
       ) {
@@ -24,5 +29,6 @@ module.exports = function check(str, bracketsConfig) {
       }
     }
   }
+
   return stack.length === 0;
 };
