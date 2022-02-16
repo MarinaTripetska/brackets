30d9aea2ee712d1a250592f3197e27ae10e5b0af
Marina Tripetska <marina.tripetskaya@gmail.com>
2022-02-16 17:45:10 +0100

    feat: resolved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = bracketsConfig.map((pair) => pair[0]);
+  const closeBrackets = bracketsConfig.map((pair) => pair[1]);
+
+  const stack = [];
+  for (let i = 0; i < str.length; i += 1) {
+    let curSymbol = str[i];
+
+    if (openBrackets.includes(curSymbol)) {
+      stack.push(curSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (
+        closeBrackets.indexOf(curSymbol) === openBrackets.indexOf(topElement)
+      ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
